365d874b2487190ddadbae9bac97ebcc74a3a1a4
Jerome Flesch <jflesch@gmail.com>
2013-06-30 21:34:00 +0200

    add dependency: pyxdg
    Signed-off-by: Jerome Flesch <jflesch@gmail.com>

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ Let the machine do most of the work.
           "Whoosh",
           "pyinsane >= 1.0.2",
           "pyocr",
+          "pyxdg",
           # "PyGObject",  # doesn't work with virtualenv
           # Missing due to the use of gobject introspection:
           # - gtk
